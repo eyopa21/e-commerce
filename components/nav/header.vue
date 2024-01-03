9ebbd907dc09de098e1e4e759b40b9987830236e
joba <jobtennis21@gmail.com>
2024-01-03 23:46:02 +0300

    active class link added in nav

--- components/nav/header.vue
+++ components/nav/header.vue
@@ -14,13 +14,19 @@ const layout = useLayout()
                 <h3 class="md:w-[160px]">LOGO</h3>
             </div>
             <nav class="items-center hidden gap-6 text-sm text-gray-500 lg:flex lg:px-4">
-                <NuxtLink to="/">Home</NuxtLink>
-                <NuxtLink to="/products">Products</NuxtLink>
-                <NuxtLink to="/categories">Categories</NuxtLink>
-                <NuxtLink to="/contact">Contact</NuxtLink>
+                <NuxtLink to="/" class="hover:border-b-2 border-primary" active-class="text-primary">Home</NuxtLink>
+                <NuxtLink to="/products" class="hover:border-b-2 border-primary" active-class="text-primary">Products
+                </NuxtLink>
+                <NuxtLink to="/categories" class="hover:border-b-2 border-primary" active-class="text-primary">Categories
+                </NuxtLink>
+                <NuxtLink to="/contact" class="hover:border-b-2 border-primary" active-class="text-primary">Contact
+                </NuxtLink>
 
-                <NuxtLink class="lg:hidden" to="/wishlist" :prefetch="false">Wishlist</NuxtLink>
-                <NuxtLink class="lg:hidden" to="/auth/login" :prefetch="false">My Account</NuxtLink>
+                <NuxtLink class="lg:hidden hover:border-b-2 border-primary" to="/wishlist" :prefetch="false"
+                    active-class="text-primary">Wishlist</NuxtLink>
+                <NuxtLink class="lg:hidden hover:border-b-2 border-primary" to="/auth/login" :prefetch="false"
+                    active-class="text-primary">My Account
+                </NuxtLink>
             </nav>
 
             <div class="flex gap-4 justify-end items-center md:w-[160px] flex-1 ml-auto">
@@ -28,13 +34,15 @@ const layout = useLayout()
                 <div class="relative cursor-pointer sm:hidden" title="Cart" @click="layout.showSearch = !layout.showSearch">
                     <Icon name="ion:search-outline" size="20" class="mr-1 md:mr-0" />
                 </div>
-                <NuxtLink to="/wishlist" title="Wishlist">
+                <NuxtLink to="/wishlist" title="Wishlist" class="hover:border-b-2 border-primary"
+                    active-class="text-primary">
                     <Icon name="ion:heart-outline" size="20" />
                 </NuxtLink>
-                <NuxtLink to="/my-account" title="Account">
+                <NuxtLink to="/my-account" title="Account" class="hover:border-b-2 border-primary"
+                    active-class="text-primary">
                     <Icon name="ion:person-outline" size="20" />
                 </NuxtLink>
-                <CartToggleCart />
+                <CartToggleCart class="hover:border-b-2 border-primary" />
             </div>
         </div>
         <Transition name="scale-y" mode="out-in">
